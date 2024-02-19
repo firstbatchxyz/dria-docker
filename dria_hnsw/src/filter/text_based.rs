a0e1b98f0541427aea7531b4e506142622e8b50d
erhant <erhany96@gmail.com>
2024-02-19 18:24:11 +0300

    add score to query result

--- dria_hnsw/src/filter/text_based.rs
+++ dria_hnsw/src/filter/text_based.rs
@@ -1,13 +1,13 @@
-use probly_search::{Index, QueryResult};
-use probly_search::score::bm25;
 use probly_search::score::ScoreCalculator;
-use std::borrow::Cow;
+use probly_search::score::{bm25, zero_to_one};
+use probly_search::{Index, QueryResult};
 use serde_json::{json, Value};
+use std::borrow::Cow;
 
 pub struct Wiki {
     pub id: usize,
     pub title: String,
-    pub text: String
+    pub text: String,
 }
 
 fn tokenizer(s: &str) -> Vec<Cow<str>> {
@@ -22,19 +22,22 @@ fn text_extract(d: &Wiki) -> Vec<&str> {
     vec![d.text.as_str()]
 }
 
-pub fn create_index_from_docs(index: &mut Index<usize>, query:&str, metadata: Vec<Value>)-> Vec<Value> {
-
+pub fn create_index_from_docs(
+    index: &mut Index<usize>,
+    query: &str,
+    metadata: Vec<Value>,
+) -> Vec<Value> {
     let mut wikis = Vec::new();
     let mut query_results = Vec::new();
-
+    let mut ids = Vec::new();
     let mut iter = 0;
 
     for (_, value) in metadata.iter().enumerate() {
-
-
+        println!("{:?}", value);
         let text = value["metadata"]["text"].as_str();
         let title = value["metadata"]["title"].as_str();
-        let id = value["id"].as_u64().unwrap() as usize;
+        let id = value["metadata"]["id"].as_str().unwrap();
+        let id_doc = value["id"].as_u64().unwrap() as usize;
         let url = value["metadata"]["url"].as_str();
 
         let t = text.unwrap().to_string();
@@ -45,11 +48,11 @@ pub fn create_index_from_docs(index: &mut Index<usize>, query:&str, metadata: Ve
             let wiki = Wiki {
                 id: iter,
                 title: title.clone(),
-                text: sentence.to_string()
+                text: sentence.to_string(),
             };
             wikis.push(wiki);
             query_results.push(json!({"id":id, "title":title.clone(), "text":sentence.to_string(), "url":url.unwrap()}));
-
+            ids.push(id_doc);
             iter += 1;
         }
     }
@@ -64,9 +67,9 @@ pub fn create_index_from_docs(index: &mut Index<usize>, query:&str, metadata: Ve
 
     let results = index.query(query, &mut bm25::new(), tokenizer, &vec![1., 1.]);
     let mut results_as_wiki = vec![];
-    for res in results.iter(){
-        results_as_wiki.push(query_results[res.key].clone());
+    for res in results.iter() {
+        let val = json!({"id": ids[res.key], "metadata": query_results[res.key].clone(), "score": res.score});
+        results_as_wiki.push(val);
     }
-    return results_as_wiki
+    return results_as_wiki;
 }
-
